887765ab9173f372f898932cfb1462b420561f52
kozerka <kozerka@gmail.com>
2023-06-17 14:39:59 +0200

    Clear comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,15 @@
 const path = require('path');
-// importuję bibliotękę [path] z [node.js]
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// importuję odpowiedni plugin
 const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
 	entry: {
 		client: ['whatwg-fetch', './src/js/client.js'],
 		admin: ['whatwg-fetch', './src/js/admin.js'],
 	},
-	// definiuje pliki wejściowe
-	// posiadające swoje identyfikatory [chunks]
+
 	output: {
 		path: path.resolve(__dirname, 'build'),
-		// definiuje ścieżką wyjściową
 		filename: '[name].min.js',
-		// definiuję nazwę pliku wyjściowego
 	},
 	devtool: 'inline-source-map',
 	devServer: {
@@ -25,23 +20,13 @@ module.exports = {
 		rules: [
 			{
 				test: /\.js$/,
-				// określam jakie pliki
-				// będą brane pod uwagę
 				exclude: /node_modules/,
-				// określam wykluczenia
 				use: 'babel-loader',
-				// określam jaki [loader]
-				// ma być wykorzystany
 			},
 			{
 				test: /\.css$/,
-				// określam jakie pliki
-				// będą brane pod uwagę
 				exclude: /node_modules/,
-				// określam wykluczenia
 				use: ['style-loader', 'css-loader'],
-				// określam jaki [loader]
-				// ma być wykorzystany
 			},
 
 			{
@@ -58,19 +43,13 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
-			// wskazuje plik źródłowy
 			filename: 'index.html',
-			// określan nazwę dla pliku
 			chunks: ['client'],
-			// wskazuje plik do podpięcia
 		}),
 		new HtmlWebpackPlugin({
 			template: './src/admin.html',
-			// wskazuje plik źródłowy
 			filename: 'admin.html',
-			// określan nazwę dla pliku
 			chunks: ['admin'],
-			// wskazuje plik do podpięcia
 		}),
 		new CopyPlugin({
 			patterns: [
@@ -82,4 +61,3 @@ module.exports = {
 		}),
 	],
 };
-// eksportuję ustawienia dla webpack-a
